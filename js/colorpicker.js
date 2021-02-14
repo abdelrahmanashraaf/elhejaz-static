213994c6bde3e667c266b53ead9716d822bf4357
abdelrahmanashraaf <abdelrahmanashraf767@gmail.com>
2021-02-14 17:01:00 +0200

    after finishing color pickers

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -1,9 +1,13 @@
 $(document).ready(() => {
 
-    const I = getComputedStyle(document.documentElement).getPropertyValue('--colors-count');
-    const colors = [];
+    const I = getComputedStyle(document.documentElement).getPropertyValue('--colors-count') + 1;
+    const colorsPrimary = [];
+    const colorsSecondary = [];
+    const colorsBackground = [];
     for (let index = 0; index < I; index++) {
-        colors[index] = getComputedStyle(document.documentElement).getPropertyValue('--color-' + index)
+        colorsPrimary[index] = getComputedStyle(document.documentElement).getPropertyValue('--colorprimary-' + index)
+        colorsSecondary[index] = getComputedStyle(document.documentElement).getPropertyValue('--colorsecondary-' + index)
+        colorsBackground[index] = getComputedStyle(document.documentElement).getPropertyValue('--colorbackground-' + index)
     }
 
     // clear all class
@@ -23,7 +27,7 @@ $(document).ready(() => {
     for (let index = 0; index < I; index++) {
 
         $("#cp_btn_" + index).first().on('click', (e) => {
-            document.documentElement.style.setProperty('--primary-color', colors[index]);
+            document.documentElement.style.setProperty('--primary-color', colorsPrimary[index]);
             removeBorder();
             $(e.target).addClass("cp-btn-selected");
         });
@@ -41,7 +45,7 @@ $(document).ready(() => {
     for (let index = 0; index < I; index++) {
 
         $("#csecondary_btn_" + index).first().on('click', (e) => {
-            document.documentElement.style.setProperty('--secondary-color', colors[index]);
+            document.documentElement.style.setProperty('--secondary-color', colorsSecondary[index]);
             $(".no-border").removeClass("csecondary-btn-selected");
             $(e.target).addClass("csecondary-btn-selected");
         });
@@ -52,13 +56,13 @@ $(document).ready(() => {
     picker_3.on('input', (e) => {
         removeBorder();
         document.documentElement.style.setProperty('--background-color', e.target.value);
-        $(picker_1).addClass("cback-btn-selected");
+        $(picker_3).addClass("cback-btn-selected");
     });
 
     for (let index = 0; index < I; index++) {
 
         $("#cback_btn_" + index).first().on('click', (e) => {
-            document.documentElement.style.setProperty('--background-color', colors[index]);
+            document.documentElement.style.setProperty('--background-color', colorsBackground[index]);
             $(".no-border").removeClass("cback-btn-selected");
             $(e.target).addClass("cback-btn-selected");
         });
@@ -74,7 +78,7 @@ $(document).ready(() => {
     const picker_5 = $("#ctext_2").first();
     picker_5.on('input', (e) => {
         removeBorder();
-        document.documentElement.style.setProperty('var(--darktexts-color)', e.target.value);
+        document.documentElement.style.setProperty('--darktexts-color', e.target.value);
         $(picker_5).addClass("cback-btn-selected");
     });
 
--- js/main.js
+++ js/main.js
@@ -340,19 +340,6 @@ anime.timeline({ loop: false })
     });
 
 
-// $(document).ready(function() {
-//     $('.counter-value').each(function() {
-//         $(this).prop('Counter', 0).animate({
-//             Counter: $(this).text()
-//         }, {
-//             duration: 3500,
-//             easing: 'swing',
-//             step: function(now) {
-//                 $(this).text(Math.ceil(now));
-//             }
-//         });
-//     });
-// });
 
 $('#counter').waypoint(function() {
     $('.milestones-section .aos-animate').ready(function() {
